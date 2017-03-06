a39bd3be5ac71591a2f822719935c50e495d4fb1
Li Han <lisirrx@gmail.com>
2017-03-06 16:58:39 +0800

    addmin

--- wishwall/settings.py
+++ wishwall/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-   'DEFAULT_PERMISSION_CLASSES': (
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
 }
